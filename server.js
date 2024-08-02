4616f348a1eec1761ea1f5521497069029594b27
Aniket Chavan <anikettchavan103@gmail.com>
2024-08-02 21:03:16 +0530

    1st commit

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route handler for the default home page
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
