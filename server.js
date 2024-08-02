5b0859a6623f0e7f978c712e63ecfe4329cb9887
Aniket Chavan <anikettchavan103@gmail.com>
2024-08-02 21:24:16 +0530

    a

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 // Define a route handler for the default home page
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.json('Hello, world!');
 });
 
 // Start the server
